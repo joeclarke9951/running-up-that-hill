6f85c03e0a19b39e4029c75f195951f651ce5c8c
Joe Clarke <joeclarke9951@gmail.com>
2026-09-02 00:23:25 +0100

    settings menu remembers look sensitivity

--- Scripts/pause_menu.gd
+++ Scripts/pause_menu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
 		get_tree().paused = !get_tree().paused
--- Scripts/settings_menu.gd
+++ Scripts/settings_menu.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+func _ready() -> void:
+	$Camera2D/VBoxContainer/SensitivitySlider.value = SettingsManager.look_sensitivity
 
 func _on_back_button_button_down() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")

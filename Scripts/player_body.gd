1f3ab5880739d401394c5f9159ea305c90ae047b
Joe Clarke <joeclarke9951@gmail.com>
2026-09-01 16:38:11 +0100

    added basic player movement and a rudimentary level mesh

---
+++ Scripts/player_body.gd
@@ -0,0 +1,28 @@
+extends CharacterBody3D
+
+var gravity = -9.8
+@export var move_speed = 10
+@export var jump_speed = 5
+@export var sprint_speed = 18
+@export var look_sensitivity = 0.005
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _physics_process(delta: float) -> void:
+	velocity.y += gravity * delta
+	var input = Input.get_vector("left","right","forward","backward")
+	var direction = transform.basis * Vector3(input.x, 0, input.y)
+	var speed = sprint_speed if Input.is_action_pressed("sprint") else move_speed
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	
+	move_and_slide()
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = jump_speed
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * look_sensitivity)
+		$PlayerCamera.rotate_x(-event.relative.y * look_sensitivity)
+		$PlayerCamera.rotation.x = clampf($PlayerCamera.rotation.x, -deg_to_rad(70), deg_to_rad(70))

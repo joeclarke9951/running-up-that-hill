50fb409c8100958b55b47344dfeb489d7547f188
Joe Clarke <joeclarke9951@gmail.com>
2026-09-04 16:22:52 +0100

    trash is launched downwards rather than dropped

--- Scenes/trash_spawn.gd
+++ Scenes/trash_spawn.gd
@@ -1,13 +1,12 @@
 extends Node3D
 
-# preload the trash object(s)
-# write a function to spawn them in at the spawn position
 # eventually either make several spawns or a range of spawns
 	# different objects will have different dimensions, if you use a Range you'll need to account for sizes
 	# different Ranges that can each only spawn one object? and choose between them when it's time to spawn something?
 	# calculate the range dynamically before the object appears?
 
-var trash:Array[PackedScene] = [null]
+@export var launch_speed = -5000
+var trash:Array[PackedScene] = [null] # save each of the trash models here and pick one at random
 
 func _ready() -> void:
 	trash[0] = preload("res://PackedScenes/basic-trash-object.tscn")
@@ -17,7 +16,8 @@ func _process(delta: float) -> void:
 		spawn_trash(0)
 
 func spawn_trash(num:int):
-	var t:Node3D = trash[num].instantiate()
+	var t:RigidBody3D = trash[num].instantiate()
 	t.position = Vector3(0, 10, -5)
 	t.rotation = Vector3(randf(),randf(),randf())
 	add_child(t)
+	t.apply_impulse(Vector3(0,launch_speed,0))
